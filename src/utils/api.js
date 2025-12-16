971a860a73e96cae459019841365763cf2582699
sammetamaheshbabu <70381126+sammetamaheshbabu@users.noreply.github.com>
2025-12-16 22:09:53 +0530

    some ifx

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,24 @@
-export const apiBase = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3300';
+export const apiBase = process.env.REACT_APP_API_BASE_URL;
 
 export async function apiFetch(path, options = {}) {
-	const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
-	const headers = {
-		'Content-Type': 'application/json',
-		...(options.headers || {}),
-	};
-	if (token && !headers.Authorization) headers.Authorization = `Bearer ${token}`;
+  const token =
+    typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+  const headers = {
+    'Content-Type': 'application/json',
+    ...(options.headers || {}),
+  };
+  if (token && !headers.Authorization)
+    headers.Authorization = `Bearer ${token}`;
 
-	const res = await fetch(`${apiBase}${path}`, { ...options, headers });
-	return res;
+  const res = await fetch(`${apiBase}${path}`, { ...options, headers });
+  return res;
 }
 
 export const api = {
-	get: (path, options={}) => apiFetch(path, { method: 'GET', ...options }),
-	post: (path, body, options={}) => apiFetch(path, { method: 'POST', body: JSON.stringify(body), ...options }),
-	put: (path, body, options={}) => apiFetch(path, { method: 'PUT', body: JSON.stringify(body), ...options }),
-	del: (path, options={}) => apiFetch(path, { method: 'DELETE', ...options }),
+  get: (path, options = {}) => apiFetch(path, { method: 'GET', ...options }),
+  post: (path, body, options = {}) =>
+    apiFetch(path, { method: 'POST', body: JSON.stringify(body), ...options }),
+  put: (path, body, options = {}) =>
+    apiFetch(path, { method: 'PUT', body: JSON.stringify(body), ...options }),
+  del: (path, options = {}) => apiFetch(path, { method: 'DELETE', ...options }),
 };
